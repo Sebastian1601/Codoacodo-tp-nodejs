e37a1f32a418a889b4dec93a25781285e07a6a19
David Giordano Escritorio PC <davidseba.giordano@gmail.com>
2024-05-12 12:18:31 -0300

    agregado icono en minima resolucion con transparencia - no funciona js de ubicacion de marcadores. revisar -

--- JS/mapa.js
+++ JS/mapa.js
@@ -1,34 +1,47 @@
 /* para este punto, ya está cargado los datos de cada lugar en el archivo mapa_datos.js */
 
 
-
+// creo primero la variable que va a guardar la información del mapa
 
 let mapaActual;
-let marcadores=[]
 
-const crearCadaMarcador = (nombre,coord) => {
-    let marcador_ = new google.maps.Marker({
-        position:coord,
-        map:mapaActual,
-        icon:"./Media/img/img_icono_mapa.png",
-})
-    marcadores.push(marcador_)
+// creo el array que va a guardar la información de los distintos lugares
+
+let marcadores = []
+
+/* esta funcion recibe 2 parametros, nombre y coord, y los usa para crear un marcador de google, con las coordenadas, en el mapa actual y guardarlos en el array
+ */
+const crearCadaMarcador = (nombre, coord) => {
+  let html = `<h3>${nombre}</h3>`
+  let marcador_ = new google.maps.Marker({
+    position: coord,
+    map: mapaActual,
+    icon: "./Media/img/img_icono_mapa_32.png",
+  })
+  google.maps.event.addListener(marcador_, "click", () => {
+    infoBubble.setContent(html);
+    infoBubble.open(mapaActual, marcador_);
+  marcadores.push(marcador_)
+  })
 }
 
+
+/* esta funcion se usa para con cada elemento del objeto parking, obtener estos datos y crear un marcador nuevo invocando la funcion definida anteriormente
+ */
 const crearMarcadoresEstacionamientos = () => {
-    parking.forEach(lugar=>{
-            let coord = new google.maps.LatLng(lugar.lat, lugar.lng);
-            let name = lugar.nombre;
-            console.log(name + " " + coord);
-            crearCadaMarcador(name, coord);
-})
-
+  parking.forEach(lugar => {
+    let coord = new google.maps.LatLng(lugar.lat, lugar.lng);
+    let name = lugar.nombre;
+    console.log(name + " " + coord);
+    crearCadaMarcador(name, coord);
+  })
 }
 
 
-
+/* esta funcion se usa para inicializar el mapa de google, y definir un punto inicial, en este caso buenos buenosAires, toma el div de clase mapa__main y le mete el mapa como elemento interno
+ */
 async function initMap() {
-    let buenosAires = { lat: -34.592, lng: -58.451}
+  let buenosAires = { lat: -34.592, lng: -58.451 }
   const { Map } = await google.maps.importLibrary("maps");
 
   mapaActual = new Map(document.getElementById("mapa__main"), {
@@ -37,10 +50,13 @@ async function initMap() {
   });
 
   const marcadorInicial = new google.maps.Marker({
-    map:mapaActual,
+    map: mapaActual,
     position: buenosAires,
-})
-crearMarcadoresEstacionamientos();
+  })
+  /*   aqui al inicializar, creo los marcadores de los elementos que tenga en el archivo "mapa_datos.js" */
+  crearMarcadoresEstacionamientos();
+
+  var infoBubble = new google.maps.InfoWindow();
 }
 
 initMap();
