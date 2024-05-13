07b037ad21039599b123d225f79876bdf784a522
David Giordano Escritorio PC <davidseba.giordano@gmail.com>
2024-05-12 22:23:22 -0300

    mapa funcional con la ubicacion de 3 lugares teoricos

--- JS/mapa2.js
+++ JS/mapa2.js
@@ -1,45 +1,52 @@
 let mapaActual;
-let marcadores= [];
+let marcadores = [];
 
-
-const crearMarcadoresEstacionamientos = (coord, nombre) =>{
-    const marcador_ = new google.maps.Marker({
-        position:coord,
-        map: mapaActual,
-        icon:"./Media/img/img_icono_mapa_32.png",
+const mostrarListaEstacionamiento = ()=>{
+    let estacionamientoHTML = "";
+    parking.forEach(lugar=>{
+        estacionamientoHTML += `<h4>${lugar.nombre}</h4>`
     })
+    document.getElementById("estaNombres").innerHTML = estacionamientoHTML;
+}
+
+
+const crearMarcadoresEstacionamientos = (coord, nombre) => {
+    let etiqueta = `<h3> ${nombre} </h3>`
+    const marcador_ = new google.maps.Marker({
+        position: coord,
+        map: mapaActual,
+        icon: "./Media/img/img_icono_mapa_32.png",
+    })
+    google.maps.event.addListener(marcador_, "click", () => {
+        marcadorInfo.setContent(etiqueta);
+        marcadorInfo.open(mapaActual, marcador_);
+    })
+
     marcadores.push(marcador_);
 }
 
 
-const  crearMarcadoresEsta = () =>{
-    parking.forEach(esta=>{
+const crearMarcadoresEsta = () => {
+    parking.forEach(esta => {
         let coord = new google.maps.LatLng(esta.lat, esta.lng);
         let nombre = esta.nombre;
         crearMarcadoresEstacionamientos(coord, nombre);
     })
 }
 
-async function initMap(){
-    let buenosAires = {lat: -34.592, lng: -58.451};
+async function initMap() {
+    let buenosAires = { lat: -34.592, lng: -58.451 };
     const { Map } = await google.maps.importLibrary("maps");
 
-    mapaActual = new google.maps.Map(document.getElementById("mapa__main"),{
-        center:buenosAires,
-        zoom:12,
+    mapaActual = new google.maps.Map(document.getElementById("mapa__main"), {
+        center: buenosAires,
+        zoom: 12,
     })
 
-    const marcadorInicial = new google.maps.Marker({
-        position:buenosAires,
-        map:mapaActual,
-    })
+    marcadorInfo = new google.maps.InfoWindow();
 
     crearMarcadoresEsta();
-
-    const marcadorInfo = new google.maps.InfoWindow();
-    let html = `<h3>Buenos Aires</h3>`
-    marcadorInfo.setContent(html);
-    marcadorInfo.open(mapaActual, marcadorInicial);
+    mostrarListaEstacionamiento();
 }
 
 initMap();
