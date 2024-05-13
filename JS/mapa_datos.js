4c84c933d3c6c1ed17ab6f0c2e6955562dec964b
David Giordano Escritorio PC <davidseba.giordano@gmail.com>
2024-05-12 23:40:45 -0300

    Mapa funcional-falta vincular usuario con acceso a mapa, y detalles en el index(mejor presentacion)

--- JS/mapa_datos.js
+++ JS/mapa_datos.js
@@ -2,24 +2,24 @@
 
 let parking = [
     {
-        nombre:"Gosthy",
+        nombre:"Estacionamiento 1",
         lat: -34.613078,
         lng: -58.3651,
         direccion:"bla bla bla 452",
         tel: "11 5213 5681",
     },
     {
-        nombre:"Claudia" ,
+        nombre:"La cueva del Oso" ,
         lat:-34.61307517418232,
         lng:-58.37962281756501,
-        direccion:"subete a mi moto",
+        direccion:"Alderete 1521",
         tel:"11 5621 3381",
     },
     {
-        nombre:"TuEstacionamiento",
+        nombre:"Estacionamiento Chino",
         lat: -34.603544498511624,
         lng: -58.38378881103549,
-        direccion: "askjdlaksjdla 65656",
+        direccion: "Chinchulandia 6566",
         tel: "11 3352 1149",
     }
 ]
